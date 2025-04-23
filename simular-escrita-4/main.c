fed3ae732637261af9278de17547ea3a3a919158
Thalis Ferreira <thalisferreira169@gmail.com>
2025-04-23 17:25:00 -0300

    Atualização do código

--- simular-escrita-4/main.c
+++ simular-escrita-4/main.c
@@ -1,11 +1,7 @@
-// Acesso a API do Windows
 #include <windows.h>
-// Controles de botão e caixa de texto
 #include <commctrl.h>
-// Incluindo o typeText
 #include "typeText.c"
 
-// Definir o processo de criação da janela
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
@@ -15,24 +11,19 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             return 0;
 
         case WM_COMMAND:
-            // Se o botão foi clicado
             if (LOWORD(wParam) == 1)
             {
                 char text[1024];
-                // Obtém o texto da caixa de texto
                 GetWindowText(GetDlgItem(hwnd, 2), text, sizeof(text));
-                // Chama a função de digitar
                 Sleep(3000);
                 type(text);
             }
             return 0;
 
         case WM_CREATE:
-            // Criar a caixa de texto para o texto
             CreateWindow("EDIT", "", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_MULTILINE, 
                          10, 10, 380, 100, hwnd, (HMENU)2, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
 
-            // Criar o botão
             CreateWindow("BUTTON", "Simulate Typing", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
                          10, 120, 150, 30, hwnd, (HMENU)1, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
 
@@ -44,7 +35,6 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 int main()
 {
-    // Registrar a classe da janela
     const char* className = "TypingToolClass";
     WNDCLASS wc = {0};
     wc.lpfnWndProc = WindowProc;
@@ -52,16 +42,13 @@ int main()
     wc.lpszClassName = className;
     RegisterClass(&wc);
 
-    // Criar a janela
     HWND hwnd = CreateWindowEx(0, className, "Typing Tool", WS_OVERLAPPEDWINDOW,
                                CW_USEDEFAULT, CW_USEDEFAULT, 400, 200,
                                NULL, NULL, wc.hInstance, NULL);
 
-    // Mostrar a janela
     ShowWindow(hwnd, SW_SHOW);
     UpdateWindow(hwnd);
 
-    // Loop de mensagens da janela
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0))
     {
